629079d1e731f857c35c0949171f32bb05ea3167
YONATANE MEKETE <yonatanemekete33@gmail.com>
2024-08-08 02:13:03 -0700

    assets, editor UI....

--- app/(root)/documents/[id]/page.tsx
+++ app/(root)/documents/[id]/page.tsx
@@ -1,7 +1,12 @@
+import { Editor } from '@/components/editor/Editor';
 import React from 'react';
 
 const Document = () => {
-  return <div>Document</div>;
+  return (
+    <div>
+      <Editor />
+    </div>
+  );
 };
 
 export default Document;
