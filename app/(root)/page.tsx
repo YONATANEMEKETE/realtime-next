1d3642e307b9d4c6639590cf400f84050c62a994
YONATANE MEKETE <yonatanemekete33@gmail.com>
2024-08-08 22:55:41 -0700

    document list, title features

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,17 +1,23 @@
 import AddDocumentBtn from '@/components/AddDocumentBtn';
 import Header from '@/components/Header';
 import { Button } from '@/components/ui/button';
+import { getDocuments } from '@/lib/actions/room.actions';
 import { SignedIn, UserButton } from '@clerk/nextjs';
 import { currentUser } from '@clerk/nextjs/server';
 import Image from 'next/image';
 import { redirect } from 'next/navigation';
 import React from 'react';
+import Link from 'next/link';
+import { dateConverter } from '@/lib/utils';
 
 const Home = async () => {
   const clerkUser = await currentUser();
 
   if (!clerkUser) redirect('/sign-in');
-  const documents = [];
+  // const documents = [];
+  const roomDocuments = await getDocuments(
+    clerkUser.emailAddresses[0].emailAddress
+  );
 
   return (
     <main className="home-container">
@@ -24,8 +30,42 @@ const Home = async () => {
         </div>
       </Header>
 
-      {documents.length > 0 ? (
-        <div></div>
+      {roomDocuments.data.length > 0 ? (
+        <div className="document-list-container">
+          <div className="document-list-title">
+            <h3 className="text-28-semibold">All documents</h3>
+            <AddDocumentBtn
+              userId={clerkUser.id}
+              email={clerkUser.emailAddresses[0].emailAddress}
+            />
+          </div>
+          <ul className="document-ul">
+            {roomDocuments.data.map(({ id, metadata, createdAt }: any) => (
+              <li key={id} className="document-list-item">
+                <Link
+                  href={`/documents/${id}`}
+                  className="flex flex-1 items-center gap-4"
+                >
+                  <div className="hidden rounded-md bg-dark-500 p-2 sm:block">
+                    <Image
+                      src="/assets/icons/doc.svg"
+                      alt="file"
+                      width={40}
+                      height={40}
+                    />
+                  </div>
+                  <div className="space-y-1">
+                    <p className="line-clamp-1 text-lg">{metadata.title}</p>
+                    <p className="text-sm font-light text-blue-100">
+                      Created about {dateConverter(createdAt)}
+                    </p>
+                  </div>
+                </Link>
+                {/* <DeleteModal roomId={id} /> */}
+              </li>
+            ))}
+          </ul>
+        </div>
       ) : (
         <div className="document-list-empty">
           <Image
